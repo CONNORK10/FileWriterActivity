1fcd62f2a4b80cb95aadf15820fe4a4eeebe076f
Connor <CKIM7@HWEMAIL.COM>
2024-09-11 21:44:48 -0700

    Homework
    Add hidden and non-hidden files using chosen writing method; create .gitignore file

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -2,6 +2,9 @@
 import java.io.*;
 import java.nio.file.*;
 import java.nio.charset.StandardCharsets;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MyFileWriter {
     public static void main(String[] args) {
@@ -11,40 +14,34 @@ public class MyFileWriter {
         String fileName3 = "example3.txt";
         String fileName4 = "example4.txt";
         String fileName5 = "example5.txt";
-
-        // 1. Using FileWriter
-        try (FileWriter writer = new FileWriter(fileName1)) {
-            writer.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 2. Using BufferedWriter
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
-            bufferedWriter.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 3. Using FileOutputStream
-        try (FileOutputStream outputStream = new FileOutputStream(fileName3)) {
-            outputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 4. Using BufferedOutputStream
-        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(fileName4))) {
-            bufferedOutputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 5. Using Files (java.nio.file)
-        try {
-            Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
+        // 1. Using FileWriter
+        public void createPassword(String password) {
+            try {
+                File hiddenFile = new File(".password.txt");
+                FileWriter writer = new FileWriter(hiddenFile);
+                writer.write(password);
+                writer.close();
+                System.out.println("Password is" + password);
+            } catch (IOException e) {
+                System.out.println("Weak Password");
+                e.printStackTrace();
+            }
+        }
+        public void createHiddenFolder(String data) {
+            try {
+                File hiddenFolder = new File(".topsecret");
+                if (!hiddenFolder.exists()) {
+                    hiddenFolder.mkdir();
+                }
+                File regularFile = new File(hiddenFolder, "supasecret.dat");
+                FileWriter writer = new FileWriter(regularFile);
+                writer.write(data);
+                writer.close();
+                System.out.println("Hidden Folder created.");
+            } catch (IOException e) {
+                System.out.println("Error.");
+                e.printStackTrace();
+            }
+        }
 }
